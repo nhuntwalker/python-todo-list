9c2bf2d2d3a30a08bb369df19818b5128e844470
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-06-26 17:19:23 -0700

    Added a database management file, and changed a few imports away from being deprecated according to flask-migrate

--- flask/models.py
+++ flask/models.py
@@ -7,12 +7,14 @@ class Task(db.Model):
 
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(length=256, convert_unicode=True), nullable=False)
+    title = db.Column(
+        db.String(length=256, convert_unicode=True), nullable=False
+    )
     category = db.Column(db.String(length=32, convert_unicode=True))
     due_date = db.Column(db.DateTime())
     complete = db.Column(db.Boolean())
 
-    def __init__(self, title=None, category=None, due_date=None, complete=False):
+    def __init__(self, title, category=None, due_date=None, complete=False):
         """."""
         self.title = title
         self.category = category
